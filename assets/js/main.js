f4dfa22e18e57caf1ae3ee3b96f875cb0d697484
Thibaut Dumontet <dumontet.thibaut@gmail.com>
2019-08-19 12:50:20 +0200

    Js function toggle menu items when burger menu is clicked

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+// toggle navbar menu
+
+var menuBtn = document.getElementById('burger-btn');
+var menuItems = document.getElementById('menu-items');
+
+
+menuBtn.addEventListener("click", (event) => {
+  menuItems.classList.toggle("hidden");
+});
