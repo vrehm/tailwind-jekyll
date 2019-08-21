6df9656daf094aa3a90f79ef60cf7e73f33aa6f3
Vincent REHM <vincentrehm@yahoo.fr>
2019-08-21 19:19:58 +0200

    bricolage

--- _plugins/environment_variables.rb
+++ _plugins/environment_variables.rb
@@ -6,6 +6,7 @@ module Jekyll
     def generate(site)
       # Load up .env variables.
       site.config['env'] = Dotenv.load('.env')
+      site.config['env']['DEMO_VAR'] = ENV['DEMO_VAR']
       print 'dotenv variables: '
       print site.config['env']
     end
