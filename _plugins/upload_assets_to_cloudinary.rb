483879ff14c32df2e03541ad82e6629cd7a9ae6a
Vincent REHM <vincentrehm@yahoo.fr>
2019-08-23 16:31:57 +0200

    upload to cloudinary different

--- _plugins/upload_assets_to_cloudinary.rb
+++ _plugins/upload_assets_to_cloudinary.rb
@@ -38,11 +38,11 @@ uploads = {}
 
 Dir.children("assets").each do |file|
   next if file == 'img'
-  public_id = 'website_assets/website_assets/' + File.basename(file,File.extname(file))
+  public_id = File.basename(file,File.extname(file))
   unless Cloudinary::Uploader.exists?(public_id)
     uploads[:"#{file}"] = Cloudinary::Uploader.upload "assets/#{file}",
-    :tags => "website_assets",
-    :folder => "website_assets",
+    :tags => "web_assets",
+    :folder => "web_assets",
     :public_id => public_id
   end
 end
