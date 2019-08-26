563f5a082631f9305f388f6b40c1c54255594b1d
Tdum <dumontet.thibaut@gmail.com>
2019-08-26 20:26:22 +0200

    Showroom (#45)
    * Add layout to showroom section
    
    * Design for large device
    
    * includes showroom on index
    
    * refacto

--- src/main.js
+++ src/main.js
@@ -6,3 +6,21 @@ var menuItems = document.getElementById('menu-items');
 menuBtn.addEventListener("click", (event) => {
   menuItems.classList.toggle('invisible');
 });
+
+
+// parralax run with data sets
+
+window.addEventListener('scroll', function(e) {
+
+    const target = document.querySelector('.scroll');
+    const limit = target.offsetHeight + target.offsetTop;
+
+    var condition = window.pageYOffset > (target.offsetTop) && window.pageYOffset <= (limit);
+
+    var pos = (window.pageYOffset - target.offsetTop) * target.dataset.rate;
+
+    if(condition) {
+      target.style.transform = 'translate3d(0px,'+pos+'px, 0px)';
+    }
+});
+
