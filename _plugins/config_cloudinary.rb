669d4b451c28e488c91907c2bd007732952d9bcf
Vincent REHM <vincentrehm@yahoo.fr>
2019-08-26 13:45:49 +0200

    test debugging

--- _plugins/config_cloudinary.rb
+++
@@ -1,8 +0,0 @@
-# Configuration values for your account are available
-# in Cloudinary's Management Console (https://cloudinary.com/console)
-
-Cloudinary.config do |config|
-  config.cloud_name = ENV['cloudinary_cloud_name']
-  config.api_key = ENV['cloudinary_api_key']
-  config.api_secret = ENV['cloudinary_api_secret']
-end
